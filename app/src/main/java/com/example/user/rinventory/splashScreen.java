c02b4470b1455c5b85187a6c119ec68531e48f77
Mambaur Roziq Alwi <alwiroziq1998@gmail.com>
2018-05-08 11:25:55 +0700

    splashscreen

--- app/src/main/java/com/example/user/rinventory/splashScreen.java
+++ app/src/main/java/com/example/user/rinventory/splashScreen.java
@@ -1,67 +1,32 @@
 package com.example.user.rinventory;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class splashScreen extends Activity {
+
+public class splashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
-        Thread thread = new Thread(){
-            public void run (){
-                try {
-                    sleep(2000);
-                }catch (InterruptedException e){
-                    e.printStackTrace();
-                }finally {
-                    startActivity(new Intent(splashScreen.this , loginActivity.class));
-                    finish();
-                }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ss = new Intent(splashScreen.this, loginActivity.class);
+                startActivity(ss);
+                finish();
             }
-        };
-        thread.start();
+        },3000L);
+
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home){
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.optionmenu, menu);
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (item.getItemId()==R.id.in){
-//            startActivity(new Intent(getApplicationContext(), HelpActivity.class));
-//        } else if (item.getItemId() == R.id.out) {
-//            startActivity(new Intent(getApplicationContext(), HelpActivity.class));
-//        } else if (item.getItemId() == R.id.inven) {
-//            startActivity(new Intent(getApplicationContext(), Inventory.class));
-//        } else if (item.getItemId() == R.id.retur) {
-//            startActivity(new Intent(getApplicationContext(), HelpActivity.class));
-//        } else if (item.getItemId() == R.id.report) {
-//            startActivity(new Intent(getApplicationContext(), HelpActivity.class));
-//        }
-//
-//        return true;
-//    }
-
 }
